c1d3a494e3b1eef5aae30d11c280f802ff879086
Paul Jose <pauljose@live.com>
2022-03-14 12:44:08 +1100

    Extended addition to other simple calculator functionalities and also created a public static page

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# sit725-2022-t1-prac2
+sit725-2022-t1-prac2
+
+This simple calculator application exposes 4 API endpoints and each take 2 query parameters n1 and n2:
+1. http://localhost:3030/addtwonumbers
+2. http://localhost:3030/subtracttwonumbers
+3. http://localhost:3030/multiplytwonumbers
+4. http://localhost:3030/dividetwonumbers
--- server.js
+++ server.js
@@ -1,19 +1,54 @@
 const express = require("express");
 
 const app = express();
+app.use(express.static(__dirname + '/public'));
+
 const port = 3030;
 
 app.listen(port, () => {
     console.log(`App listening on port: ${port}`);
 });
 
-const addtwonumbers = (n1, n2) => {
+const addTwoNumbers = (n1, n2) => {
     return n1 + n2;
+};
+
+const subtractTwoNumbers = (n1, n2) => {
+    return n1 - n2;
+};
+
+const multiplyTwoNumbers = (n1, n2) => {
+    return n1 * n2;
+};
+
+const divideTwoNumbers = (n1, n2) => {
+    return n1 / n2;
 };
 
 app.get('/addtwonumbers', (req, res) => {
     let n1 = parseInt(req.query.n1);
     let n2 = parseInt(req.query.n2);
-    let sum = addtwonumbers(n1, n2);
+    let sum = addTwoNumbers(n1, n2);
+    res.send(sum.toString());
+});
+
+app.get('/subtracttwonumbers', (req, res) => {
+    let n1 = parseInt(req.query.n1);
+    let n2 = parseInt(req.query.n2);
+    let sum = subtractTwoNumbers(n1, n2);
+    res.send(sum.toString());
+});
+
+app.get('/multiplytwonumbers', (req, res) => {
+    let n1 = parseInt(req.query.n1);
+    let n2 = parseInt(req.query.n2);
+    let sum = multiplyTwoNumbers(n1, n2);
+    res.send(sum.toString());
+});
+
+app.get('/dividetwonumbers', (req, res) => {
+    let n1 = parseInt(req.query.n1);
+    let n2 = parseInt(req.query.n2);
+    let sum = divideTwoNumbers(n1, n2);
     res.send(sum.toString());
 });
